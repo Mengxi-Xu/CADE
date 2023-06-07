a7afff2da4cf9e91077534ff236a8a31165e3ab5
Mengxi-Xu <109797814+Mengxi-Xu@users.noreply.github.com>
2023-06-07 22:58:37 +0800

    Update autoencoder.py

--- cade/autoencoder.py
+++ cade/autoencoder.py
@@ -72,6 +72,10 @@ class Autoencoder(object):
         return:
             (ae_model, encoder_model), Model of autoencoder and model of encoder
         """
+        #这是一个使用全连接自编码器构建模型的函数。自编码器有两个部分，编码器和解码器，它们都由多个全连接层组成。
+        #编码器将输入数据映射到隐藏表示，解码器将隐藏表示解码为原始输入数据。在该函数中，使用给定的维度、
+        #激活函数和初始化方法定义了多个全连接层，构建了一个对称的自编码器模型。
+        #模型包含一个输入层、一个输出层和多个隐藏层。同时，该函数还返回了自编码器模型和编码器模型，以便后续使用。
         dims = self.dims
         act = self.act
         init = self.init
@@ -81,18 +85,23 @@ class Autoencoder(object):
         input_img = Input(shape=(dims[0],), name='input')
         x = input_img
         # internal layers in encoder
+        #对于每个隐藏层，使用for循环添加编码器的内部层。这里使用的是全连接层Dense
         for i in range(n_stacks-1):
+            #定义全连接层并添加到模型中。该层的节点数由dims[i+1]指定，激活函数由act指定，权重初始化方法由init指定。函数返回一个新的Keras层对象，并将其添加到模型中
             x = Dense(dims[i + 1], activation=act,
                       kernel_initializer=init, name='encoder_%d' % i)(x)
             # kernel_initializer is a fancy term for which statistical distribution or function to use for initializing the weights. Neural network needs to start with some weights and then iteratively update them
 
         # hidden layer, features are extracted from here, no activation is applied here, i.e., "linear" activation: a(x) = x
+        #定义自编码器的编码器部分的最后一层，并将其赋给变量encoded。这里没有激活函数，因为我们只需要提取特征并减少数据维度
         encoded = Dense(dims[-1], kernel_initializer=init,
                         name='encoder_%d' % (n_stacks - 1))(x)
+        #将编码器输出存储到实例变量self.encoded中，以便后续使用。
         self.encoded = encoded
 
         x = encoded
         # internal layers in decoder
+        #从最后一层解码器往前逐层解码
         for i in range(n_stacks-1, 0, -1):
             x = Dense(dims[i], activation=act,
                       kernel_initializer=init, name='decoder_%d' % i)(x)
@@ -112,7 +121,7 @@ class Autoencoder(object):
                        batch_size=32,
                        epochs=250,
                        loss='mse'):
-        if os.path.exists(weights_save_name):
+        if os.path.exists(weights_save_name):#判断是否已存在保存权重的文件，如果存在则无需训练纯自编码器。
             logging.info('weights file exists, no need to train pure AE')
         else:
             logging.debug(f'AE train_and_save lr: {lr}')
@@ -120,15 +129,15 @@ class Autoencoder(object):
             logging.debug(f'AE train_and_save epochs: {epochs}')
 
             verbose = self.verbose
-
+            #构建自编码器和编码器模型
             autoencoder, encoder = self.build()
-
+            #构建自编码器和编码器模型
             pretrain_optimizer = Adam(lr=lr)
-
+            #编译自编码器模型，指定优化器和损失函数。
             autoencoder.compile(optimizer=pretrain_optimizer, loss='mse')
-
+            #创建保存权重的文件所在目录。
             utils.create_parent_folder(weights_save_name)
-
+            #定义训练过程中的回调函数，用于保存最优模型
             mcp_save = ModelCheckpoint(weights_save_name,
                                     monitor='loss',
                                     save_best_only=True,
@@ -234,60 +243,63 @@ class ContrastiveAE(object):
             train_op = self.optimizer.minimize(loss, var_list=tf.trainable_variables())
 
             # Start training
-            with tf.Session(config=config) as sess:
-                loss_batch, aux_batch = [], []
+            with tf.Session(config=config) as sess:# 创建一个新的TensorFlow会话
+                loss_batch, aux_batch = [], []# 定义损失和辅助列表，用于记录训练过程中的值
                 contrastive_loss_batch, ae_loss_batch = [], []
+                sess.run(tf.global_variables_initializer()) # 初始化可训练变量
 
-                sess.run(tf.global_variables_initializer())
+min_loss = np.inf # 初始化最小损失为正无穷大
 
-                min_loss = np.inf
+# epoch training loop 进行epochs个周期的训练
+for epoch in range(epochs):
+    epoch_time = time.time()
+    
+    # split data into batches 将训练数据分为batch_count个批次
+    batch_count, batch_x, batch_y = data.epoch_batches(X_train, y_train,
+                                                           batch_size,
+                                                           similar_ratio)
+    # batch training loop 在每个批次中执行训练操作
+    for b in range(batch_count):
+        logging.debug(f'b: {b}')
+        feed_dict = {
+            input_: batch_x[b],
+            labels: batch_y[b],
+            lambda_1_tensor: lambda_1
+        }
+        # 计算损失、训练操作和其他值
+        loss1, _, aux1, contrastive_loss1, ae_loss1, \
+            dist1, encoded1 = sess.run([loss, train_op, is_same, contrastive_loss, ae_loss,
+                                        dist, ae.encoded], feed_dict=feed_dict)
 
-                # epoch training loop
-                for epoch in range(epochs):
-                    epoch_time = time.time()
-                    # split data into batches
-                    batch_count, batch_x, batch_y = data.epoch_batches(X_train, y_train,
-                                                                    batch_size,
-                                                                    similar_ratio)
-                    # batch training loop
-                    for b in range(batch_count):
-                        logging.debug(f'b: {b}')
-                        feed_dict = {
-                            input_: batch_x[b],
-                            labels: batch_y[b],
-                            lambda_1_tensor: lambda_1
-                        }
-                        loss1, _, aux1, contrastive_loss1, ae_loss1, \
-                            dist1, encoded1 = sess.run([loss, train_op, is_same, contrastive_loss, ae_loss,
-                                                        dist, ae.encoded], feed_dict=feed_dict)
+        logging.debug(f'loss1: {loss1},  aux1: {aux1}')
+        logging.debug(f'contrastive: {contrastive_loss1}, ae: {ae_loss1}')
+        logging.debug(f'epoch-{epoch} dist1[left]: {dist1[0:batch_size // 4]}')
+        logging.debug(f'epoch-{epoch} dist1[right]: {dist1[batch_size // 4:]}')
 
-                        logging.debug(f'loss1: {loss1},  aux1: {aux1}')
-                        logging.debug(f'contrastive: {contrastive_loss1}, ae: {ae_loss1}')
-                        logging.debug(f'epoch-{epoch} dist1[left]: {dist1[0:batch_size // 4]}')
-                        logging.debug(f'epoch-{epoch} dist1[right]: {dist1[batch_size // 4:]}')
+        # 记录损失和其他值到对应的列表中
+        loss_batch.append(loss1)
+        aux_batch.append(aux1)
+        contrastive_loss_batch.append(contrastive_loss1)
+        ae_loss_batch.append(ae_loss1)
 
-                        loss_batch.append(loss1)
-                        aux_batch.append(aux1)
-                        contrastive_loss_batch.append(contrastive_loss1)
-                        ae_loss_batch.append(ae_loss1)
+    if math.isnan(np.mean(loss_batch)):
+        logging.error('NaN value in loss')
 
-                    if math.isnan(np.mean(loss_batch)):
-                        logging.error('NaN value in loss')
+    # print logs each xxx epoch 每xxx个周期打印日志
+    if epoch % display_interval == 0:
+        current_loss = np.mean(loss_batch) # 计算当前损失值的平均值
+        # 打印训练日志并记录损失、辅助值和各种损失的平均值
+        logging.info(f'Epoch {epoch}: loss {current_loss} -- ' + \
+                    f'contrastive {np.mean(contrastive_loss_batch)} -- ' + \
+                    f'ae {np.mean(ae_loss_batch)} -- ' + \
+                    f'pairs {np.mean(np.sum(np.mean(aux_batch)))} : ' + \
+                    f'{np.mean(np.sum(1-np.mean(aux_batch)))} -- ' + \
+                    f'time {time.time() - epoch_time}')
+        loss_batch, aux_batch = [], [] # 清空损失和辅助列表以开始下一个周期的训练
+        contrastive_loss_batch, ae_loss_batch = [], []
 
-                    # print logs each xxx epoch
-                    if epoch % display_interval == 0:
-                        current_loss = np.mean(loss_batch)
-                        logging.info(f'Epoch {epoch}: loss {current_loss} -- ' + \
-                                    f'contrastive {np.mean(contrastive_loss_batch)} -- ' + \
-                                    f'ae {np.mean(ae_loss_batch)} -- ' + \
-                                    f'pairs {np.mean(np.sum(np.mean(aux_batch)))} : ' + \
-                                    f'{np.mean(np.sum(1-np.mean(aux_batch)))} -- ' + \
-                                    f'time {time.time() - epoch_time}')
-                        loss_batch, aux_batch = [], []
-                        contrastive_loss_batch, ae_loss_batch = [], []
-
-                        # save best weights
-                        if current_loss < min_loss:
-                            logging.info(f'updating best loss from {min_loss} to {current_loss}')
-                            min_loss = current_loss
-                            ae_model.save_weights(weights_save_name)
+        # save best weights 如果当前损失小于最小损失，则更新最佳权重并保存它们
+        if current_loss < min_loss:
+            logging.info(f'updating best loss from {min_loss} to {current_loss}')
+            min_loss = current_loss
+            ae_model.save_weights(weights_save_name)
